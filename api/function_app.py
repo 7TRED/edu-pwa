7928b6481590fbbfdbb04f6e21d7ad556f287373
Manish Singh <singhmani@microsoft.com>
2023-09-12 16:19:15 +0530

    tried to fix ChatBotTrigger

--- api/function_app.py
+++ api/function_app.py
@@ -6,34 +6,34 @@ from chatbot import get_completion_from_messages
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 
-@app.route(route="ChatBotTrigger")
-def ChatBotTrigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    response = dict()
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-            print(req_body)
-            response_content = get_completion_from_messages(
-                req_body["context"])
-            response = {
-                "role": "assistant",
-                "content": response_content
-            }
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+# @app.route(route="ChatBotTrigger")
+# def ChatBotTrigger(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
+#     response = dict()
+#     name = req.params.get('name')
+#     if not name:
+#         try:
+#             req_body = req.get_json()
+#             print(req_body)
+#             response_content = get_completion_from_messages(
+#                 req_body["context"])
+#             response = {
+#                 "role": "assistant",
+#                 "content": response_content
+#             }
+#         except ValueError:
+#             pass
+#         else:
+#             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(json.dumps(response))
-    else:
-        return func.HttpResponse(
-            json.dumps(response
-                       ),
-            status_code=200
-        )
+#     if name:
+#         return func.HttpResponse(json.dumps(response))
+#     else:
+#         return func.HttpResponse(
+#             json.dumps(response
+#                        ),
+#             status_code=200
+#         )
 
 
 @app.route(route="YTBotTrigger")
@@ -51,6 +51,36 @@ def YTBotTrigger(req: func.HttpRequest) -> func.HttpResponse:
             #     "role": "assistant",
             #     "content": response_content
             # }
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(json.dumps(response))
+    else:
+        return func.HttpResponse(
+            json.dumps(response
+                       ),
+            status_code=200
+        )
+
+
+@app.route(route="ChatBotTrigger", auth_level=func.AuthLevel.FUNCTION)
+def ChatBotTrigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    response = dict()
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+            print(req_body)
+            response_content = get_completion_from_messages(
+                req_body["context"])
+            response = {
+                "role": "assistant",
+                "content": response_content
+            }
         except ValueError:
             pass
         else:
