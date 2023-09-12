c40d973a2d060f29d1e6a7839309ff2912d7706c
Manish Singh <singhmani@microsoft.com>
2023-09-12 20:23:48 +0530

    removed dotenv

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
